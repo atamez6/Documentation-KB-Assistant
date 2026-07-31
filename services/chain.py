05cb4f36f26194f7e6fb0593c62f32dfb54a271c
atamez6 <altmzg@gmail.com>
2026-07-30 22:08:24 -0600

    FE with history

--- services/chain.py
+++ services/chain.py
@@ -14,10 +14,11 @@ SYSTEM_PROMPT = (
     "If the question is not clear, ask for clarification. "
     "you can use the context and history to understand the question, but do not assume any information that is  provided in the context or history because it could be wrong use only the information provided as vectorstore to formulate the answer. "
     "search for the answer in the vectorstore and provide the answer based on the retrieved documents; history and context are provided to help you understand the question. "
-    #"Do not assume any information that is not provided in the context."
+    "Do not assume any information that is not provided in the context."
     "Even if you already know the answer from your own training, you must NOT use that knowledge."
     "If answering the question requires connecting or inferring a relationship between pieces of information that is not explicitly stated together in the context, respond with 'I don't know.'"
-
+    "search in the history and context for the answer for questions made by the user around the same context, if you cannot find the answer in the context or history, respond with 'I don't know.'"
+    "you should always answer as the retrieved data from the vectorstore is your first option, if you cannot find the answer in the context or history, respond with 'I don't know.'"
 )
 
 prompt_template = ChatPromptTemplate.from_messages([
@@ -44,13 +45,3 @@ def ask(query: dict, history: list = None) -> tuple[str, list[dict]]:
     return response, serialized_docs
 
 
-historial_japon = [
-    HumanMessage(content="What is the capital of Japan?"),
-    AIMessage(content="The capital of Japan is Tokyo."),
-]
-
-respuesta_vecino, _ = ask(
-    {"question": "que son los chilaquiles?"},
-    history=historial_japon
-)
-print("Respuesta vecino (debe decir 'I don't know'):", respuesta_vecino)
--- ui/app.py
+++ ui/app.py
@@ -4,6 +4,7 @@ import sys, os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from services.chain import ask
 import streamlit as st
+from langchain_core.messages import HumanMessage, AIMessage
 
 
 
@@ -47,14 +48,22 @@ for msg in st.session_state.messages:
 prompt = st.chat_input("Ask me anything about your uploaded Docs and I'll retrieve most relevant information")
 
 if prompt:
+    history = []
+    for msg in st.session_state.messages[1:]:
+        if msg["role"] == "user":
+            history.append(HumanMessage(content=msg["content"]))
+        elif msg["role"] == "assistant":
+            history.append(AIMessage(content=msg["content"]))
     st.session_state.messages.append({"role":"user","content":prompt,"sources":[]})
+
+    
     with st.chat_message("user"):
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
         try:
             with st.spinner("getting info and creating a response"):
-                pre_answer, serialized_docs = ask({"question": prompt})
+                pre_answer, serialized_docs = ask({"question": prompt}, history=history)
                 answers= str(pre_answer.strip() or "(not answer to show.)")
                 sources=serialized_docs
 
